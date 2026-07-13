b348818ddaaae7a83e3e12d8b9331b1b47b7d49a
melodyL1 <92028510+melodyL1@users.noreply.github.com>
2026-07-13 19:37:22 +0800

    Delete TPPI directory

--- TPPI/models/vit_pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from TPPI.models.vit_pytorch.vit import ViT
-from TPPI.models.vit_pytorch.simple_vit import SimpleViT
-
-from TPPI.models.vit_pytorch.mae import MAE
-from TPPI.models.vit_pytorch.dino import Dino
